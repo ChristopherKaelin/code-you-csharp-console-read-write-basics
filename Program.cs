c4e0395ce736ab8f7e20a346df711d4280a4ce64
Chris 'EagleUK' Kaelin <chriskaelin67@gmail.com>
2025-11-04 19:38:10 -0500

    in-class assignment on read/write

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//  Module 1, Week 1, In Class Assignment
+//  Display some text using Console.WriteLine() or Console.Write().
+//  Wait for a user's response using Console.Read(), Console.ReadKey(), or Console.ReadLine().
+Console.WriteLine();
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+
+Console.Write("Please enter your name:  ");
+var userInput = Console.ReadLine();
+
+Console.Write("Please enter your birth year:  ");
+var birthYear = Console.ReadLine();
+
+Console.WriteLine($"Hello {userInput}, welcome to the C# programming world!");
+Console.WriteLine($"{birthYear} was a good year!");
+
+Console.WriteLine();
